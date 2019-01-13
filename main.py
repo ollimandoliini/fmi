2f2b342783ec7a23337d69d0132c2d57618670b5
Olli Kortelahti <okortelahti@gmail.com>
2019-01-13 11:51:39 +0200

    fix

--- main.py
+++ main.py
@@ -76,7 +76,6 @@ def send_daylight_message():
 
 
 if __name__ == "__main__":
-    send_daylight_message()
     schedule.every().day.at("7:00").do(send_daylight_message)
     while True:
         schedule.run_pending()
